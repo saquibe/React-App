f5eb6efdc99d332e2dc32d8c2252cb41ddec03df
saquibe <saquibe143@gmail.com>
2024-11-05 18:25:50 +0530

    maintain useState

--- my-app/src/component/FormArea.js
+++ my-app/src/component/FormArea.js
@@ -1,13 +1,37 @@
 import { Fab, Paper, TextField } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
 
 function FormArea() {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+  console.log(note);
+  function changeHandler(event) {
+    const { name, value } = event.target;
+    setNote((preValues) => {
+      return {
+        ...preValues,
+        [name]: value,
+      };
+    });
+  }
   return (
     <Paper style={{ margin: "10px 10%", padding: "25px 50px" }}>
       <form>
-        <TextField label="Title" fullWidth autoComplete="off" />
         <TextField
+          onChange={changeHandler}
+          name="title"
+          value={note.title}
+          label="Title"
+          fullWidth
+          autoComplete="off"
+        />
+        <TextField
+          onChange={changeHandler}
+          name="content"
+          value={note.content}
           style={{ marginTop: "10px" }}
           label="Content"
           multiline
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,7 @@
-import react from "react";
-import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Create a root and render the App component to it
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
